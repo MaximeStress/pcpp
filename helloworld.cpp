c7ee1b4722b94b06c3777dbe21267470cc411076
Mathis <202038812+MaximeStress@users.noreply.github.com>
2025-05-11 12:00:39 +0800

    formatting with std::println()

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,5 +2,7 @@ import std;
 
 int main() {
     std::println("Hello, World!");
+    std::println("There are {} ways I love you.", 219);
+    std::println("{} + {} = {}", 2, 4, 2+4);
     return 0;
 }
