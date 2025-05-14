2dec4508396d0be6d23e912400707cb2c308bd9b
Mathis <202038812+MaximeStress@users.noreply.github.com>
2025-05-14 14:31:53 +0800

    namespace

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,19 @@
 import std;
 
+namespace MyLibrary::network::FTP {
+    void connect() {
+        std::println(std::cout, "Connecting to FTP server...");
+    }
+}
+
+namespace MyLibrary::network::HTTP {
+    void connect() {
+        std::println(std::cout, "Connecting to HTTP server...");
+    }
+}
+
 int main() {
-    int value;
-    std::cin >> value;
-    std::println(std::cerr, "Error: {}", value);
+    MyLibrary::network::FTP::connect();
+    MyLibrary::network::HTTP::connect();
+    return 0;
 }
