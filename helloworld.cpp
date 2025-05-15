f1a452b2d9ef660a0faefe33dcdfa137c678ad8f
Mathis <202038812+MaximeStress@users.noreply.github.com>
2025-05-15 14:35:18 +0800

    variables

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,19 +1,21 @@
 import std;
 
-namespace MyLibrary::network::FTP {
-    void connect() {
-        std::println(std::cout, "Connecting to FTP server...");
-    }
-}
-
-namespace MyLibrary::network::HTTP {
-    void connect() {
-        std::println(std::cout, "Connecting to HTTP server...");
-    }
-}
-
 int main() {
-    MyLibrary::network::FTP::connect();
-    MyLibrary::network::HTTP::connect();
+    int d = 123'234'56;
+    int uni;
+    int ni {7};
+    float myFloat { 3.14f };
+    int i3 { static_cast<int>(myFloat) };
+    std::println("Number: {}, {}, {}, {}, " 
+        "Hello", d, uni, ni, myFloat);
+
+    std::println("Int:");
+    std::println("Max: {}", std::numeric_limits<int>::max());
+    std::println("Min: {}", std::numeric_limits<int>::min());
+    std::println("Lowest: {}", std::numeric_limits<int>::lowest());
+    std::println("Float:");
+    std::println("Max: {}", std::numeric_limits<float>::max());
+    std::println("Min: {}", std::numeric_limits<float>::min());
+    std::println("Lowest: {}", std::numeric_limits<float>::lowest());
     return 0;
 }
